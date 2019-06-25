76fee03394c4cdd87b9bdc4ea7f5f0b5f722715f
anaselalami <anaselalamikh@gmail.com>
2019-06-25 09:31:47 +0100

    add three methods: - getListenersOf - emitAll - removeListenersOf

--- README.md
+++ README.md
@@ -20,14 +20,24 @@ eventBus.addListeners('key', _ => {
   // do something
 });
 
+// get all listeners keys that has `key` in there name, like `key-dsfy`, `key-something`, ...
+eventBus.getListenersOf('key');
+
 // to remove a listener
 eventBus.removeListener('key');
 
 // to remove all listeners
 eventBus.removeAllListeners();
 
+// to remove all listeners with name has with `key` in there name
+eventBus.removeListenersOf('key');
+
 // to emit a event
 eventBus.emit('key');
+
+// to emit a event to all subscribed event that has `key` in there name 
+// `key` `key-test` `key-something` all theses event will be triggered
+eventBus.emitAll('key');
 
 // check if listener exist
 eventBus.listenerExist('key');
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ let eventBus = new EventEmitter();
 
 let EventInterface = {
 
+  listenerExist(key) {
+    return eventBus.listeners(key).length > 0;
+  },
+  
   addListeners(key, callback) {
     eventBus.addListener(key, callback);
   },
@@ -12,15 +16,24 @@ let EventInterface = {
       eventBus.addListener(key, callback);
     }
   },
+
+  getListenersOf(key) {
+    let obj = eventBus._subscriber._subscriptionsForType;
+    let listeners = Object.keys(obj).filter((k) => { return ~k.indexOf(key) });
+    return listeners;
+  },
   
   emit(key, data) {
     eventBus.emit(key, data);
   },
   
-  listenerExist(key) {
-    return eventBus.listeners(key).length > 0;
+  emitAll(key, data) {
+    let listeners = EventInterface.getListenersOf(key);
+    listeners.map(_key => {
+      EventInterface.emit(_key, data);
+    });
   },
-  
+
   removeListener(key) {
     if(this.listenerExist(key)) {
       delete eventBus._subscriber._subscriptionsForType[key];
@@ -29,6 +42,12 @@ let EventInterface = {
   
   removeAllListeners() {
     eventBus.removeAllListeners();
+  },
+
+  removeListenersOf(key) {
+    EventInterface.getListenersOf(key).map(key => {
+      EventInterface.removeListener(key);
+    });
   },
   
   getInstance() {
